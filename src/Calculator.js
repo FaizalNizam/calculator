1ca12f89381a8c41c70e7e90a1214b81aef88455
Appstation React Native <appstationreactnative@Faizal-Mac-mini.local>
2022-06-10 18:22:28 +0530

    dynamic height

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,6 +1,8 @@
-import { View, Text, SafeAreaView, FlatList, Pressable, Alert, Switch } from 'react-native'
+import { View, Text, SafeAreaView, FlatList, Pressable, Alert, Switch,Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import { Data } from './DATA'
+
+const {height}=Dimensions.get('screen')
 
 const Calculator = () => {
 
@@ -54,10 +56,10 @@ const Calculator = () => {
             <Text style={{ color:theme?'white': item.color, fontSize: 20 }}>{item.button}</Text>
         </Pressable>
     )
-
+    //console.log('height',height*0.06);
     return (
         <SafeAreaView style={{ flex: 1,backgroundColor: theme ? '#0f0f0f' : 'white' }}>
-            <View style={{height:800, backgroundColor: theme ? '#0f0f0f' : 'white'}}>
+            <View style={{height:height*0.948, backgroundColor: theme ? '#0f0f0f' : 'white'}}>
                 <Switch
                     thumbColor={theme?'white':'black'}
                     trackColor={theme?'#8b8f8c':'#babfbc'}
@@ -66,12 +68,12 @@ const Calculator = () => {
                     value={theme}
                     style={{ marginLeft: 180 }}
                 />
-                <View style={{ height: 300, justifyContent: 'flex-end', alignItems: 'flex-end', paddingBottom: 50, paddingRight: 40 }}>
+                <View style={{ height: height*0.356, justifyContent: 'flex-end', alignItems: 'flex-end', paddingBottom: 50, paddingRight: 40 }}>
                     <Text style={{ fontSize: 30,color:theme?'white':'black'}}>{data}</Text>
                     <Text style={{ fontSize: 40,color:theme?'white':'black'}}>{result}</Text>
                 </View>
 
-                <View style={{ backgroundColor: theme ? '#0f0f0f' : '#ebedeb', height: 500, borderRadius: 40 }}>
+                <View style={{ backgroundColor: theme ? '#0f0f0f' : '#ebedeb', height: height*0.593, borderRadius: 40 }}>
                     <FlatList
                         data={Data}
                         renderItem={render}
